d0a33892bf7d48a0f0fd3be9e859aaa0d5019844
Clelia (Astra) Bertelli <clelia@runllama.ai>
2025-08-20 19:31:47 +0200

    requester email

--- src/utils/supabase/edge-function.ts
+++ src/utils/supabase/edge-function.ts
@@ -14,7 +14,7 @@ export async function sendEmails(date: Date, time: string, email: string, name: 
      } else {
         console.log(data)
         const { data: data1, error: error1 } = await supabase.functions.invoke("send-email", {
-        body: {html: `<h3>Hey there!</h3>\n<p>You are receiving this email because ${name} is trying to book a spot in your calendar on ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} at ${time} (Central European Time). Please let them know whether that works for you or not!</p>\n<br>\n<p>All the best,</p>\n<br>\n<p>BookMe Bot</p>\n`, to: "astraberte9@gmail.com", subject: "You Got a BookMe Request!"}
+        body: {html: `<h3>Hey there!</h3>\n<p>You are receiving this email because ${name} (${email}) is trying to book a spot in your calendar on ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} at ${time} (Central European Time). Please let them know whether that works for you or not!</p>\n<br>\n<p>All the best,</p>\n<br>\n<p>BookMe Bot</p>\n`, to: "astraberte9@gmail.com", subject: "You Got a BookMe Request!"}
      })
      if (error1) {
         console.log("An error occurred:", error1)
